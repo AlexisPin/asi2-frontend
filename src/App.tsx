b6788976da6b2fb85ae7cb10f4688abd2c4914dc
AdrienLapalus <adri.lapalus@gmail.com>
2023-12-08 08:22:26 +0100

    feat(app): add route

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-import { useState } from 'react'
-import './App.css'
-import { Input } from './components/form/input'
+import "./App.css";
+import { Input } from "./components/form/input";
+import { Navigate, Routes } from "react-router";
+import { Route } from "react-router-dom";
 
 function App() {
-
   return (
-    <>
-     <Input />
-    </>
-  )
+    <Routes>
+      <Route index path="/" element={<Navigate to={"/login"} replace />} />
+
+      <Route path="/login" element={<Input />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React, { lazy } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const App = lazy(() => import("./App"));
+
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route path={"*"} element={<App />} />)
+);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
